a45318299c020296fd0ae308725cab1eb02e2c75
BRASS <mr.brn.lewis@outlook.com>
2021-05-23 10:56:06 +0800

    created first tests for employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,11 +2,24 @@ const Employee = require("../lib/Employee.js");
 
 require("../lib/Employee.js");
 
-describe ("Employee Class", () =>{
+describe ("Employee Class", () =>
+{
     //test stuff
     test("Return error if no arguments provided for employee", () => {
         //actual test
+      expect(new Employee()).toEqual(Error);
+    });
+    test("Return error if atleast one argument is undefined for employee", () => {
+      expect(new Employee("Brendan")).toEqual(Error);
+    });
+    test("Return employee object when all argurments provided", () => {
+      //create a mock employee object to test against the returned object from Employee
 
-        expect(new Employee()).toEqual(Error);
+      const testEmployee = {
+        name: "Brendan",
+        id: 1,
+        email: "mr.brn.lewis@gmail.com"
+      };
+      expect(new Employee("Brendan",1,"mr.brn.lewis@gmail.com")).toEqual(testEmployee);
     });
 });
