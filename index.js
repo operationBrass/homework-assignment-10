223bda6148bcfe16dc95d0f3e448a71cd1bab5f0
BRASS <mr.brn.lewis@outlook.com>
2021-05-24 10:44:21 +0800

    the questions are working and sending calls for new objects to be created

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 //import modules for employee types
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
-const engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 
 //node imports
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Manager = require("./lib/Manager");
+
+
 
 //global varibles
 let objArray = new Array();
 
+//create a NEW manager
+function generateManager(name, id, email, officeNum) 
+{
+    objArray.push(new Manager(name, id, email, officeNum));
+}
 
-// setup questions for user to enter team manager detials
+//create NEW engineer
 
-const questions = [{
-    
+function genEng(name, id, email, gitUser) {
+    objArray.push(new Engineer(name, id, email, gitUser));
+}
+
+//create a NEW intern
+function genIntern(name, id, email, school) {
+    objArray.push(new Intern(name, id, email, gitUser));
+}
+
+
+// hold list of questions as array of objects and make available to index.js module
+
+const managerQ = 
+[{
     type: "input", 
     name:"name", 
     message:"Team Managers Name:",
-
 },
 {
     type: "input", 
     name:"id", 
     message:"Employee ID:",
-  
 },                   
 {
     type: "input", 
@@ -36,54 +52,122 @@ const questions = [{
     type: "input", 
     name:"officeNum", 
     message:"Office Number:",
-},
-{ // user selection for additional employee creation... add seperator then exit choice
+}];
+
+const subMenuQ =
+[{ 
+    // user selection for additional employee creation... add seperator then exit choice
     type: "list", 
-    name:"subMenu", 
-    message:"Add new team members?", 
+    name:"options", 
+    message:"Create new team member?", 
     choices: ["Create Engineer","Create Intern", new inquirer.Separator(), "Exit"]
 }];
+
+const internQ = [{
+    type: "input", 
+    name:"name", 
+    message:"Interns Name:",
+},
+{
+    type: "input", 
+    name:"id", 
+    message:"Employee ID:",
+},                   
+{
+    type: "input", 
+    name:"email", 
+    message:"Email Address:"
+},
+{
+    type: "input", 
+    name:"school", 
+    message:"Attending School:",
+}]
+
+const engineerQ = [{
+    type: "input", 
+    name:"name", 
+    message:"Engineers Name:",
+},
+{
+    type: "input", 
+    name:"id", 
+    message:"Employee ID:",
+},                   
+{
+    type: "input", 
+    name:"email", 
+    message:"Email Address:"
+},
+{
+    type: "input", 
+    name:"github", 
+    message:"Github Username:",
+}];
+
+// begin application
 
 startSeq();
 
 // starting function to kick things off
-function startSeq()
-{
-inquirer.prompt(questions)
-    .then(answers =>
-{
+function startSeq() {
+    inquirer.prompt(managerQ)
+    .then(answers => 
+        {
+            objArray.push(new Manager(answers.name, answers.id, answers.email, answers.officeNum));
+            subMenu();
+        })
+    .catch(error => {
+        console.log("error",error);
+    });
+}
 
-    // send call to create a manager object
-    objArray.push(genMan(answers.name,answers.id,answers.email,answers.officeNum));
-    console.log(objArray);
+function subMenu()
+{
+    inquirer.prompt(subMenuQ)
+    .then(answers => 
+        {
+            switch (answers.options)
+            {
+                case "Create Intern":
+                internMenu();
+                break;
+                case "Create Engineer":
+                engineerMenu();
+                break;
+                default:
+                console.log("exit");
+            }
+        })
+    .catch(error => {
+        console.log("error");
+    });
 
-    switch (answers.subMenu)
+}
+
+function internMenu() 
+{
+    inquirer.prompt(internQ)
+
+    .then(answers => 
     {
-        case "Exit":
-        break;
-        case "Engineer":
-        break;
-        case "Intern":
-        break;
-    }
-});
+        objArray.push(new Intern(answers.name, answers.id, answers.email, answers.school));
+        subMenu();
+    })
+    .catch(error => 
+    {
+        console.log("error");
+    });
 }
-
-//create a NEW manager
-function genMan(name,id,email,officeNum)
+function engineerMenu()
 {
- objArray.push(new Manager(name,id,email,officeNum));
-}
-
-//create NEW engineer
-
-function genEng(name,id,email,gitUser)
-{
-    objArray.push(new Engineer(name,id,email,gitUser));
-}
-
-//create a NEW intern
-function genIntern(name,id,email,school)
-{
-    objArray.push(new Intern(name,id,email,gitUser));
+    inquirer.prompt(engineerQ)
+    .then(answers => 
+        {
+            objArray.push(new Engineer(answers.name, answers.id, answers.email, answers.gitUser));
+            subMenu();
+        })
+    .catch(error => {
+        console.log("error");
+    });
 }
